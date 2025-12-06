3eff561a763cdd1ea84beab08ba16302cfbe96ea
Jakub Balys <kbalys444@gmail.com>
2025-12-06 21:15:39 +0100

    bug fixed

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def main():
     while True:
         log_state()
         for event in pygame.event.get():
-            if event.type == pygame.QUIT():
+            if event.type == pygame.QUIT:
                 return
         screen.fill("black")
         pygame.display.flip()
